ffae9e50ec44cfbd22cd2f88442f24d71ef79f1e
Tim Patterson <tpatterson@sailthru.com>
2023-05-03 18:44:56 +1200

    Add some perspective

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,11 +5,13 @@ const Wrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(${p => p.width * 2 + 1}, min(${p => p.cellWidth}vw, ${p => p.cellHeight}vh));
     grid-template-rows: repeat(${p => p.height}, min(${p => p.cellWidth * 2 / 1.1547}vw, ${p => p.cellHeight * 2 / 1.1547}vh));
+    transform: perspective(1200px) rotateX(18deg);
+    transform-origin: bottom;
 `;
 
 function Board() {
-    const width = 15;
-    const height = 9;
+    const width = 20;
+    const height = 13;
     const cells = [];
     for (var i=0; i < height; i++) {
         for (var j=0; j < width * 2; j+=2) {
